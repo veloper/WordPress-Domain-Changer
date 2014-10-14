3e8300eb5fde43faaf097ae3964e103998910262
Daniel Doezema <dan@doezema.com>
2014-10-14 07:26:43 -0400

    t-dog

--- run-tests.sh
+++ run-tests.sh
@@ -4,14 +4,14 @@ PATH=~/.composer/vendor/bin:$PATH
 if [ ! -f ~/.composer/vendor/bin/phpunit ]; then
   echo "Running composer installation..."
   php composer.phar global install
-  php composer global require 'phpunit/phpunit=4.1.*'
+  php composer.phar global require 'phpunit/phpunit=4.1.*'
 fi
 
 WPDC_PATH=`pwd`
 
 echo "Running PHPUnit suite..."
 cd "$WPDC_PATH/tests/phpunit"
-phpunit --configuration ./phpunit.xml
+php composer.phar --configuration ./phpunit.xml
 
 PHPUNIT_EXIT_CODE=$?
 echo "PHPUnit Exit Code: $PHPUNIT_EXIT_CODE"
@@ -20,7 +20,7 @@ echo "Running RSpec/Capybara suite..."
 cd "$WPDC_PATH/tests/rspec"
 bundle install
 bundle config build.nokogiri --use-system-libraries
-bundle exec rspec ./spec/feature/* --format='nested' --color
+bundle exec rspec ./spec/feature/wordpress_versions_spec.rb --format='nested' --color
 
 RSPEC_EXIT_CODE=$?
 echo "RSpec Exit Code: $PHPUNIT_EXIT_CODE"
