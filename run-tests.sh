b1519f8452667926287e8137478b998cd141deed
Daniel Doezema <dan@doezema.com>
2014-10-17 03:42:57 -0400

    mooooore the travis

--- run-tests.sh
+++ run-tests.sh
@@ -1,14 +1,6 @@
 #!/bin/bash -l
 
-source ~/.bashrc
-
 PATH=~/.composer/vendor/bin:$PATH
-
-if [ ! -f ~/.composer/vendor/bin/phpunit ]; then
-  echo "Running composer installation..."
-  php composer.phar global install
-  php composer.phar global require 'phpunit/phpunit=4.1.*'
-fi
 
 echo "Running PHPUnit suite..."
 phpunit --configuration phpunit.xml
@@ -17,8 +9,6 @@ PHPUNIT_EXIT_CODE=$?
 echo "PHPUnit Exit Code: $PHPUNIT_EXIT_CODE"
 
 echo "Running RSpec/Capybara suite..."
-echo `pwd`
-bundle install
 bundle exec rake
 
 RSPEC_EXIT_CODE=$?
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,8 @@ require 'open-uri'
 require 'pathname'
 require 'thread'
 
+require 'support/word_press_util'
+
 ROOT_PATH              = File.expand_path("../../", __FILE__)
 WPDC_PATH              = File.join(ROOT_PATH, "wpdc")
 SPEC_PATH              = File.join(ROOT_PATH, "spec")
@@ -37,9 +39,6 @@ end
 RSpec.configure do |config|
 
   config.before(:suite) do
-    puts "* Downloading supported wordpress versions"
-    WordPressUtil.download_archives!
-
     puts "* Copying WPDC to #{DUMMY_WPDC_PATH}"
     FileUtils.mkdir_p DUMMY_WPDC_PATH
     FileUtils.cp_r WPDC_PATH, DUMMY_PATH
@@ -64,80 +63,6 @@ RSpec.configure do |config|
   end
 
   config.include Capybara::DSL
-
-end
-
-class WordPressUtil
-
-  def self.work_queue!(queue, &block)
-    5.times.map do |i|
-      Thread.new do
-        Thread.current[:id] = i.to_s
-        def puts(output); super("Worker #{Thread.current[:id]}: " + output) end
-        begin
-          while x = queue.pop(true) do
-            block.call(x)
-          end
-        rescue ThreadError
-          puts "Done"
-        end
-      end
-    end.map(&:join)
-  end
-
-  def self.archives_path
-    Pathname.new(File.join(SUPPORT_PATH, "wordpress_archives")).tap(&:mkpath)
-  end
-
-  def self.archives
-    archives_path.children
-  end
-
-  def self.iversion(version)
-    version << ".0" if(version.split('.').length == 2)
-    version << ".0" if(version.split('.').length == 3)
-    version.to_s.split('.').reverse.each_with_index.reduce(0) do |seed, (part, i)|
-      calc = part.to_i
-      calc = calc * (10 ** (i * 3)) if i > 0
-      seed += calc
-      seed
-    end
-  end
-
-  def self.download_archives!
-
-    # Non Multi & >= version 2
-    html = open("https://wordpress.org/download/release-archive/").read
-    urls = html.scan(/(http\:\/\/wordpress\.org\/wordpress\-[0-9\.]+?\.zip)["']{1}/).flatten.select do |url|
-      version = url[/-([0-9\.]+?).zip/, 1]
-      iversion(version) > iversion("2.0.1.0")
-    end
-
-    # Get Download URLS
-    downloaded_files = archives_path.children.map(&:basename).map(&:to_path)
-    queue = urls.reduce(Queue.new) do |queue, url|
-      if !downloaded_files.include?(File.basename(url = url.gsub("http:", "https:")))
-        queue << url
-      end
-      queue
-    end
-
-    # Download Archive
-    if queue.length > 0
-      work_queue!(queue) do |url|
-        file_path = archives_path.join(File.basename(url))
-        puts "* Downloading: #{url} ---> TO ---> #{file_path.to_path}"
-        begin
-          File.open(file_path.to_path, "wb") do |file|
-            open(url, "rb") {|download| file.write(download.read) }
-          end
-        rescue
-          file_path.delete if file_path.exists?
-        end
-      end
-    end
-
-  end
 
 end
 
