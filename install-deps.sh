b13e1f208eda57ca5b62151b2051bc28000df0ca
Daniel Doezema <dan@doezema.com>
2014-10-18 17:48:32 -0400

    travis

--- install-deps.sh
+++ install-deps.sh
@@ -1,8 +1,12 @@
 #!/bin/bash -l
 
+
 echo "Composer Install..."
 php composer.phar global install
 php composer.phar global require 'phpunit/phpunit=4.1.*'
+
+echo "Bundle Install..."
+bundle install
 
 echo "WordPress Versions Download..."
 ruby -e 'require File.expand_path("../spec/spec_helper.rb", __FILE__); WordPressUtil.download_archives!'
